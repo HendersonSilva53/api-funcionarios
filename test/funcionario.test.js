a0b06f24afefae576a02a2cba70e50d218bd3627
HendersonSilva <hendersonsilva_@hotmail.com>
2026-07-03 18:04:12 -0300

    Api-Funcionarios

---
+++ test/funcionario.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+
+const app = require("../app");
+
+//Escrita de um teste unitário
+
+describe("API de Funcionarios", ()=>{
+    it("Deve cadastrar um novo funcionario", async ()=>{
+        const resposta = await request(app)
+        .post("/funcionarios")
+        .send({
+            nome: "Maria",
+            cargo: "Analista"
+        })
+
+        expect(resposta.statusCode.toBe(201));
+
+        expect(resposta.body.nome).toBe("Maria");
+
+        expect(resposta.body.cargo).toBe("Analista");
+    })
+})
