cf7340fcfda483c87d631657f3f98755ebc024a6
HendersonSilva <hendersonsilva_@hotmail.com>
2026-07-07 20:17:55 -0300

    Teste unitario

--- test/funcionario.test.js
+++ test/funcionario.test.js
@@ -37,3 +37,20 @@ describe("API de Funcionarios", ()=>{
         expect(resposta.body.cargo).toBe("Desenvolvedor");
     })
 })
+
+describe("API de Funcionarios", ()=>{
+    it("Deve cadastrar um novo funcionario", async ()=>{
+        const resposta = await request(app)
+        .post("/funcionarios")
+        .send({
+            nome: "Henderson Silva",
+            cargo: "Desenvolvedor"
+        })
+
+        expect(resposta.statusCode).toBe(201);
+
+        expect(resposta.body.nome).toBe("Henderson Silva");
+
+        expect(resposta.body.cargo).toBe("Desenvolvedor");
+    })
+})
