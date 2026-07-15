66b59adcd53053131e5c1478033973f6167d4a65
HendersonSilva <hendersonsilva_@hotmail.com>
2026-07-15 15:14:59 -0300

    feat: adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routes = require("./routes/funcionarioRoutes");
 const sequelize = require("./config/database");
+const cors = require("cors");
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
